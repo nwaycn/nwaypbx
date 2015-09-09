729b66ca14d9367f4a00a3aec7ff604544980ca8
nwaycn <lihao@nway.com.cn>
2015-09-09 09:05:47 +0800

    added nway freeswitch ext files

---
+++ src/nwaypbx/nwaypbx/fs_ext.cpp
@@ -0,0 +1,204 @@
+/*************************************************************************
+based for NwayPBX
+Copyright (C) 2015-, Li hao <lihao@nway.com.cn>
+License： GPL
+author: Li hao
+email: lihao@nway.com.cn
+The Initial Developer of the Original Code is
+Li hao<lihao@nway.com.cn>
+Portions created by the Initial Developer are Copyright (C)
+the Initial Developer. All Rights Reserved.
+Contributor(s):
+**************************************************************************/
+
+#include "fs_ext.h"
+#ifdef WIN32
+#include "../unistd.h"
+#else
+#include "unistd.h"
+#endif
+
+
+int check_event_body(const char * eventbody, char * dtmfbuf, int maxdtmf, char * endchar, int * channel_execute)
+{
+	char tmp[128];
+	unsigned int i, len;
+
+	if (eventbody == NULL) return 0;
+	len = strlen(eventbody);
+	if (len>64) len = 64;
+	strncpy(tmp, eventbody + strlen("Event-Name: "), len);
+	tmp[len] = 0;
+	for (i = 0; i<strlen(tmp); i++) if (tmp[i] == '\n' || tmp[i] == '\r') { tmp[i] = 0; break; }
+
+	//disp_msg("Event-Name:[%s]", tmp);
+	if (strcmp(tmp, "CHANNEL_EXECUTE_COMPLETE") == 0)
+	{
+		if (*channel_execute) return 1;
+		//disp_msg("CHANNEL_EXECUTE_COMPLETE invalid");
+		return 0;
+	}
+	if (strcmp(tmp, "CHANNEL_EXECUTE") == 0) *channel_execute = 1;
+	if (strcmp(tmp, "CHANNEL_HANGUP") == 0) return -98;
+	//disp_msg("event_body:%s", eventbody);
+	//if(strcmp(tmp,"CHANNEL_PROGRESS")==0)
+
+	//if(_strnicmp(eventbody,"Event-Name: DTMF",strlen("Event-Name: DTMF"))==0)
+	if (strncasecmp(eventbody, "Event-Name: DTMF", strlen("Event-Name: DTMF")) == 0)
+	{
+		char*p;
+
+		if (p = (char *)strstr(eventbody, "DTMF-Digit: "))
+		{
+			strcpy(tmp, p + strlen("DTMF-Digit: "));
+			for (i = 0; i<strlen(tmp); i++)if (tmp[i] == '\n' || tmp[i] == '\r') { tmp[i] = 0; break; }
+			if (strcmp(tmp, "%23") == 0) strcpy(tmp, "#");
+			//disp_msg("dtmf:[%s]", tmp);
+			if (dtmfbuf && strlen(dtmfbuf)<64) strcat(dtmfbuf, tmp);
+			if (strlen(dtmfbuf) >= maxdtmf) return 2;
+			len = strlen(endchar);
+			//支持最大3个结束按键
+			if (len>0) if (tmp[0] == endchar[0]) return 3;
+			if (len>1) if (tmp[0] == endchar[1]) return 3;
+			if (len>2) if (tmp[0] == endchar[2]) return 3;
+		}
+	}
+	
+	return 0;
+}
+
+int check_acm_body(const char * eventbody, char * uuid)
+{
+	char tmp[4096];
+	unsigned int i, len;
+	char*p;
+	if (eventbody == NULL) return 0;
+	len = strlen(eventbody);
+	if (len>64) len = 64;
+	strncpy(tmp, eventbody + strlen("Event-Name: "), len);
+	tmp[len] = 0;
+	for (i = 0; i<strlen(tmp); i++) if (tmp[i] == '\n' || tmp[i] == '\r') { tmp[i] = 0; break; }
+
+	//disp_msg("Event-Name:[%s]", tmp);
+
+	//	//disp_msg("event_body:%s", eventbody);
+	if (strcmp(tmp, "CHANNEL_PROGRESS") != 0)
+	{
+		return 0;
+	}
+
+
+
+
+	if (p = (char *)strstr(eventbody, "Unique-ID: "))
+	{
+		strcpy(tmp, p + strlen("Unique-ID: "));
+		for (i = 0; i<strlen(tmp); i++)if (tmp[i] == '\n' || tmp[i] == '\r') { tmp[i] = 0; break; }
+
+		//disp_msg("acm UUID:[%s]", tmp);
+		strcpy(uuid, tmp);
+	}
+
+	return 1;
+}
+
+int check_event(esl_handle_t * handle, int timer)
+{
+	int done = 0, c = 0;
+	esl_status_t status;
+	time_t exp = 0;
+	char dtmf[128];
+	int res = 0;
+	int  channel_execute = 0;
+	dtmf[0] = 0;
+	printf("%s,%d check_event\n", __FILE__, __LINE__);
+	while ((status = esl_recv_timed(handle, 1000)) != ESL_FAIL)
+	{
+		c++;
+		//disp_msg("Waiting 1 seconds events.\n");
+		if (timer>0 && c >= timer) { res = 100; break; }
+
+		if (status == ESL_SUCCESS)
+		{
+			const char *type = esl_event_get_header(handle->last_event, "content-type");
+
+			if (type)
+			{
+				if (strcasecmp(type, "text/disconnect-notice") == 0)
+				{
+					const char *dispo = esl_event_get_header(handle->last_event, "content-disposition");
+					//disp_msg("Got a disconnection notice dispostion: [%s]", dispo ? dispo : "");
+					if (dispo && strcmp(dispo, "linger") == 0)
+					{
+						res = -99;
+						break;
+					}
+				}
+				if (strcasecmp(type, "text/event-plain") == 0)
+				{
+					const char *eventbody = esl_event_get_body(handle->last_event);
+					if ((res = check_event_body(eventbody, dtmf, 1, "#", &channel_execute)))
+					{
+						//disp_msg("check_event_body res=%d.",res);
+						if (res<0)	break;
+					}
+				}
+
+			}
+		}
+	}
+	//disp_msg("check_event res=%d",res);
+	return res;
+}
+
+bool get_uuid(esl_handle_t * pHandle, char * uuid)
+{
+	esl_send_recv(pHandle, "api create_uuid\n\n");
+	if (pHandle->last_sr_event && pHandle->last_sr_event->body)
+	{
+		//disp_msg("uuid:[%s]****************************************************\n", pHandle->last_sr_event->body);
+		strcpy(uuid, pHandle->last_sr_event->body);
+		return true;
+		//strcpy(uuid,(handle.last_sr_event->body)+strlen("+OK Job-UUID: "));
+	}
+	else
+	{
+		//disp_msg("[%s] last_sr_reply\n", pHandle->last_sr_reply);
+		return false;
+	}
+	return false;
+}
+
+bool nway_hangup(esl_handle_t * pHandle, const char * uuid)
+{
+	char szCmd[2048] = { 0 };
+	sprintf(szCmd, "bgapi uuid_kill %s\n\n", uuid);
+	esl_send_recv(pHandle, szCmd);// "bgapi originate {originate_timeout=30}sofia/internal/1005%192.168.1.102 &park()\n\n");
+								  //	uuid[0]=0;
+	if (pHandle->last_sr_event && pHandle->last_sr_event->body)
+	{
+		printf("[%s]\n", pHandle->last_sr_event->body);
+		//	strcpy(uuid,(handle.last_sr_event->body)+strlen("+OK Job-UUID: "));
+		return true;
+	}
+	else
+	{
+		printf("[%s] last_sr_reply\n", pHandle->last_sr_reply);
+		return false;
+	}
+}
+
+bool nway_isdigit(const char * str)
+{
+	int len = strlen(str);
+	bool bResult = true;
+	for (int i = 0; i< len; i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			bResult = false;
+			break;
+		}
+	}
+	return bResult;
+}
---
+++ src/nwaypbx/nwaypbx/fs_ext.h
@@ -0,0 +1,86 @@
+/*************************************************************************
+based for NwayPBX
+Copyright (C) 2015-, Li hao <lihao@nway.com.cn>
+License： GPL
+author: Li hao
+email: lihao@nway.com.cn
+The Initial Developer of the Original Code is
+Li hao<lihao@nway.com.cn>
+Portions created by the Initial Developer are Copyright (C)
+the Initial Developer. All Rights Reserved.
+Contributor(s):
+**************************************************************************/
+
+#ifndef __NWAY_PBX_FREESWITCH_EXT__
+#define __NWAY_PBX_FREESWITCH_EXT__
+#include <esl.h>
+//在这里有一部分代码是移自于厦门-余洪涌某一个公开的源代码文件中
+
+int check_event_body(const char*eventbody, char*dtmfbuf, int maxdtmf, char* endchar, int *channel_execute);
+
+
+int check_acm_body(const char*eventbody, char*uuid);
+int check_event(esl_handle_t *handle, int timer);
+
+int check_last_event(esl_handle_t *handle, int timer);
+
+int check_acm_event(esl_handle_t *handle, int timer, char* uuid);
+
+//播音之后的取按键检查函数
+//返回: <0 表示对方挂机，=100 表示按键最大时间结束条件满足 =101 表示两个按键间的间隔时间结束条件条件满足,=3表示 按键结束条件,比如"#" 满足，=2表示 最大按键个数结束条件 满足
+//参数:handle:会话handle
+//filename:语音文件名称，多个文件以分号";"隔开,文件名称可以带.wav,扩展名,假如没有,默认是.pcm扩展名.可指定路径,假如没有,默认是语音程序的./data/system目录下
+//enddtmf:按键结束条件,比如"#"表示按#号结束输入,""表示没有结束按键条件//支持最大3个结束按键 比如 EndDtmf="*0#" 表示按 0，* 或者#都可以结束
+//MaxDtmf:最大按键个数结束条件,0表示没有按键个数结束条件
+//MaxTimer:按键最大时间结束条件,单位秒,0表示没有最大时间结束条件
+//TwoDtmfTimer:两个按键间的间隔时间结束条件,单位秒,0表示没有两个按键间的间隔时间结束条件
+//outdtmf:收到的用户的按键(输出参数),包括结束按键的条件,比如"#"
+//其他说明:假如只有播音,不收取按键 设置：MaxDtmf=0
+int check_play_dtmf_event(esl_handle_t *handle, char*enddtmf, int MaxDtmf, int MaxTimer, int TwoDtmfTimer, char*outdtmf);
+//播音取按键
+//返回: <0 表示对方挂机，=100 表示按键最大时间结束条件满足 =101 表示两个按键间的间隔时间结束条件条件满足,=3表示 按键结束条件,比如"#" 满足，=2表示 最大按键个数结束条件 满足
+//参数:handle:会话handle
+//filename:语音文件名称，多个文件以分号";"隔开,文件名称可以带.wav,扩展名,假如没有,默认是.pcm扩展名.可指定路径,假如没有,默认是语音程序的./data/system目录下
+//EndDtmf:按键结束条件,比如"#"表示按#号结束输入,""表示没有结束按键条件//支持最大3个结束按键 比如 EndDtmf="*0#" 表示按 0，* 或者#都可以结束
+//MaxDtmf:最大按键个数结束条件,0表示没有按键个数结束条件
+//MaxTimer:按键最大时间结束条件,单位秒,0表示没有最大时间结束条件
+//TwoDtmfTimer:两个按键间的间隔时间结束条件,单位秒,0表示没有两个按键间的间隔时间结束条件
+//dtmf:收到的用户的按键(输出参数),包括结束按键的条件,比如"#"
+//invalidfile:输入无效时的提示语音
+//Maxfailure:最多尝试次数
+//说明:假如只有播音,不收取按键 设置：MaxDtmf=0
+//#define  playdir  "d:/data/system"
+int play_get_dtmf(esl_handle_t *handle, const char*filename0, const char* invalidfile, const char*EndDtmf, int MaxDtmf, int MaxTimer, int TwoDtmfTimer, int MaxFailure, char*outdtmf);
+//录音之后的取按键检查函数
+//返回: <0 表示对方挂机，=100 表示按键最大时间结束条件满足 =101 表示两个按键间的间隔时间结束条件条件满足,=3表示 按键结束条件,比如"#" 满足，=2表示 最大按键个数结束条件 满足
+//参数:handle:会话handle
+//filename:语音文件名称，多个文件以分号";"隔开,文件名称可以带.wav,扩展名,假如没有,默认是.pcm扩展名.可指定路径,假如没有,默认是语音程序的./data/system目录下
+//enddtmf:按键结束条件,比如"#"表示按#号结束输入,""表示没有结束按键条件//支持最大3个结束按键 比如 EndDtmf="*0#" 表示按 0，* 或者#都可以结束
+//MaxDtmf:最大按键个数结束条件,0表示没有按键个数结束条件
+//MaxTimer:按键最大时间结束条件,单位秒,0表示没有最大时间结束条件
+//TwoDtmfTimer:两个按键间的间隔时间结束条件,单位秒,0表示没有两个按键间的间隔时间结束条件
+//outdtmf:收到的用户的按键(输出参数),包括结束按键的条件,比如"#"
+//其他说明:假如只有播音,不收取按键 设置：MaxDtmf=0
+
+int check_record_dtmf_event(esl_handle_t *handle, char*enddtmf, int MaxDtmf, int MaxTimer, int TwoDtmfTimer, char*outdtmf);
+// 录 音取按键函数
+// 返回: <0 表示对方挂机，=100 表示按键最大时间结束条件满足 =101 表示两个按键间的间隔时间结束条件条件满足,=3表示 按键结束条件,比如"#" 满足，=2表示 最大按键个数结束条件 满足
+// 参数:handle:会话handle
+// uuid： 会话的id
+// filename:语音文件名称，多个文件以分号";"隔开,文件名称可以带.wav,扩展名,假如没有,默认是.pcm扩展名.可指定路径,假如没有,默认是语音程序的./data/system目录下
+// EndDtmf:按键结束条件,比如"#"表示按#号结束输入,""表示没有结束按键条件//支持最大3个结束按键 比如 EndDtmf="*0#" 表示按 0，* 或者#都可以结束
+// MaxDtmf:最大按键个数结束条件,0表示没有按键个数结束条件
+// MaxTimer:按键最大时间结束条件,单位秒,0表示没有最大时间结束条件
+// TwoDtmfTimer:两个按键间的间隔时间结束条件,单位秒,0表示没有两个按键间的间隔时间结束条件
+// outdtmf:收到的用户的按键(输出参数),包括结束按键的条件,比如"#"
+int record_get_dtmf(esl_handle_t *handle, char*uuid, char*filename, char*EndDtmf, int MaxDtmf, int MaxTimer, int TwoDtmfTimer, char*outdtmf);
+
+bool get_uuid(esl_handle_t* pHandle, char* uuid);
+
+bool nway_hangup(esl_handle_t* pHandle, const char* uuid);
+
+bool nway_isdigit(const char* str);
+#endif
+
+
+
